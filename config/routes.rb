dd5ab5d33fe57ee5912d7649a4a0b1f3056465d7
LucasJego <jego.lucas@gmail.com>
2019-11-06 17:42:16 +0100

    Ajout appli initiale

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Le programme redirige la page d'accueil vers la page qui indique tous les événements (méthode 'index' du controller 'events_controller')
+  root 'events#index'
+
+  resources :events
+
+end
